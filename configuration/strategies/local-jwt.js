c1d1c5b8f941f1967d826bf006c5ce251a485305
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2017-04-28 19:53:50 +0800

    add Passport JSON web tokens authentication strategy

---
+++ configuration/strategies/local-jwt.js
@@ -0,0 +1,28 @@
+/**
+ * JWT authentication strategy file
+ * Created by Kudzai Gopfa on 3/5/2017.
+ */
+
+var passport = require('passport'),
+    JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt,
+    config = require('./env/development.js'),
+    User = require('mongoose').model('Users');
+
+module.exports = function() {
+    passport.use(new JwtStrategy(config.jwtOptions, function(jwt_payload, done) {
+        User.findOne({
+            id: jwt_payload.sub
+        }, function(err, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            }
+            else {
+              done(null, false);
+            }
+        });
+    }));
+};
