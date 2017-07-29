a9b49383b9ce7dbfc85054e7bd3e3af768178228
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2017-07-29 10:45:51 +0800

    UPDATE schema file paths

--- config/mongoose.js
+++ config/mongoose.js
@@ -16,9 +16,9 @@ const config      =  require('./env/development.js');
 module.exports = function() {
     let db = mongoose.connect(config.DB_URL, { useMongoClient: true });
 
-    require('./../application/models/users.server.model.js');
-    require('./../application/models/images.server.model.js');
-    require('./../application/models/comments.server.model.js');
+    require('./../app/models/users.server.model.js');
+    require('./../app/models/images.server.model.js');
+    require('./../app/models/comments.server.model.js');
 
     return db;
 };
