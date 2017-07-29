11b244525191cb060962c9b9a9d83cc7642697cf
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2017-07-29 13:24:42 +0800

    UPDATE likes to check for duplication

--- app/controllers/likes.server.controller.js
+++ app/controllers/likes.server.controller.js
@@ -27,35 +27,39 @@ let getErrorMessage = function(err) {
 };
 
 exports.like = function(req, res) {
-    let image        =  new Image(req.image);
-    image.liked_by   =  req.user;
+    let image        =  req.image;
+    let conditions   =  {
+                          _id: image._id,
+                          'liked_by._id': {$ne: req.user._id}
+                        };
+    let update       =  {
+                          $addToSet: { liked_by: req.user }
+                        }
 
-    image.save(function(err) {
-        if(err) {
-            return res.status(400).send(getErrorMessage(err));
-        } else res.json(image);
-    });
-};
-
-exports.list = function(req, res) {
-    Image.find({_id: escape(req.image._id)}).populate('liked_by', 'username').sort('-created').exec(function(err, likes) {
-        if(err) {
-            return res.status(400).send({
-                message: getErrorMessage(err)
+    Image.findByIdAndUpdate(conditions, update , {new: true}, function(err, image) {
+        if (err) {
+            return res.status(401).send({
+                message: err/*getErrorMessage(err)*/
             });
-        } else res.json(likes);
+        } else return res.json(image);
     });
 };
 
 exports.unlike = function(req, res) {
-    let image_id = req.image._id;
-    let liker_id = req.user._id;
+    let image        =  req.image;
+    let conditions   =  {
+                          _id: image._id,
+                          'liked_by._id': {$ne: req.user._id}
+                        };
+    let update       =  {
+                          $pull: { liked_by: req.user._id }
+                        }
 
-    like.findByIdAndUpdate(image_id, { $pullAll: {liked_by: [liker_id] }, {new: true}, function(err, image) {
+    Image.findByIdAndUpdate(conditions, update , {new: true}, function(err, image) {
         if (err) {
             return res.status(401).send({
-                message: getErrorMessage(err)
+                message: err/*getErrorMessage(err)*/
             });
-        } else res.json(image);
+        } else return res.json(image);
     });
 };
