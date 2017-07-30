7be87b57743144009e5a7eca1f0d267187bdcbd1
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2017-07-30 09:33:02 +0800

    UPDATE comment controller

--- app/controllers/comments.server.controller.js
+++ app/controllers/comments.server.controller.js
@@ -10,7 +10,6 @@
 * Module dependencies
 */
 
-const Comment    =  require('mongoose').model('Comment');
 const Image      =  require('mongoose').model('Images');
 
 let getErrorMessage = function(err) {
@@ -28,63 +27,39 @@ let getErrorMessage = function(err) {
 };
 
 exports.create = function(req, res) {
-    let comment         = new Comment(req.body);
-    comment.posted_by   =  req.user;
-    comment.image       =  req.image;
+    let image        =  req.image;
 
-    comment.save(function(err) {
-        if(err) {
-            return res.status(400).send(getErrorMessage(err));
-        } else res.json(comment);
-    });
-};
+    image.comments.push({
+                            comment: req.body.comment,
+                            comment_by: req.user
+                        });
 
-exports.update = function(req, res) {
-    let comment      = req.comment;
-    comment.comment  = req.body.comment;
-    comment.updated  = new Date();
-
-    if(escape(comment.posted_by) === escape(req.user._id)) {
-        comment.save(function(err) {
-            if(err) {
-                return res.status(400).send({
-                    message: getErrorMessage(err)
-                });
-            } else res.json(comment);
-        });
-    } else res.status(401).send({message: "Action only available to author"});
-};
-
-exports.list = function(req, res) {
-    Comment.find({image: req.image._id}).sort('-created').exec(function(err, comments) {
-        if(err) {
-            return res.status(400).send({
-                message: getErrorMessage(err)
+    image.save(function(err, image_u) {
+        if (err) {
+            return res.status(401).send({
+                message: err/*getErrorMessage(err)*/
             });
-        } else res.json(comments);
+        } else return res.json(image_u);
     });
 };
 
 exports.commentByID = function(req, res, next, id) {
-    Comment.findOne({_id: id}).exec(function(err, comment) {
-        if(err) {
-            return res.status(400).send({
-                message: getErrorMessage(err)
-            });
-        }
-        if(!comment) {
-            return next(new Error('Failed to load article' + id));
-        }
-        else {
+    let image = req.image;
+    image.comments.forEach(function(comment) {
+        if(escape(comment._id) == id) {
             req.comment = comment;
+            return next();
         }
-        next();
     });
 };
 
+exports.read = function(req, res) {
+    res.json(req.comment);
+};
+
 exports.checkUser = function(req, res, next) {
-    let comment = req.comment;
-    if(!req.user.id === comment.posted_by.id) {
+    let image = req.image;
+    if(!(escape(req.user.id) === escape(image.comments.comment_by._id))) {
         res.status(403).send({
             message: 'Action only available to author'
         });
@@ -92,13 +67,11 @@ exports.checkUser = function(req, res, next) {
 };
 
 exports.delete = function(req, res) {
-    let comment = req.comment;
+    let image        =  req.image;
 
-    comment.remove(function(err) {
-        if(err) {
-            return res.status(400).send({
-                message: getErrorMessage(err)
-            });
-        } else res.json(comment);
+    image.update({ $pullAll: {'comments': [req.comment] } }, {new: true}, function(err, image_u) {
+        if(err)
+            return res.json(err);
+        else return res.json(image_u);
     });
 };
