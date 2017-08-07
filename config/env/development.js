a432741ad617ae53fa16b0ff53dbd1b49c175a80
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2017-08-07 14:24:41 +0800

    UPDATE database url

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 
 module.exports = {
     JWT_SECRET  : "%51LLY:5C13NCE;8R0/",
-    DB_URL      : 'mongodb://localhost/dapper_dev'
+    DB_URL      : 'mongodb://developer:1L0V3P1350MUCH@ds049084.mlab.com:49084/heroku_47xrxmjw/dapper_dev'
 };
