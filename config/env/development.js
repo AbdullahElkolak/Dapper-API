b29ccb5a9a386a522ab12f573915840a30087479
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2017-08-10 12:57:49 +0800

    ADD aws-sdk configuration

--- config/env/development.js
+++ config/env/development.js
@@ -1,8 +1,11 @@
 
 module.exports = {
-    JWT_SECRET     : process.env.JWT_SECRET,
-    DB_URL         : process.env.DAPPER_DEV_MONGO_URI,
-    S3_BUCKET      : process.env.S3_BUCKET,
-    AWS_ACCESS_KEY : process.env.AWS_ACCESS_KEY,
-    AWS_SECRET_KEY : process.env.AWS_SECRET_KEY
+    JWT_SECRET            : process.env.JWT_SECRET,
+    DB_URL                : process.env.DAPPER_DEV_MONGO_URI,
+    S3_BUCKET             : process.env.S3_BUCKET,
+    AWS                   : {
+        "accessKeyId"     : process.env.AWS_ACCESS_KEY_ID,
+        "secretAccessKey" : process.env.AWS_SECRET_ACCESS_KEY,
+        "region"          : 'ap-southeast-1'
+    }
 };
