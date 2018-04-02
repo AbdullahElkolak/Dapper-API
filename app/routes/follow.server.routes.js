342e12aa02e4615e348bac6b517d87ca6b7ccbe4
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2018-04-02 08:57:25 +0800

    ADD follow record id to request

--- app/routes/follow.server.routes.js
+++ app/routes/follow.server.routes.js
@@ -18,10 +18,13 @@ module.exports = function(app) {
     app.route('/api/users/:profileId/follow')
         .post(passport.authenticate('jwt', {session: false}), follows.follow)
         .delete(passport.authenticate('jwt', {session: false}), follows.unfollow);
+    
+    app.delete('/api/users/:profileId/unfollow/:followRecord', passport.authenticate('jwt', {session: false}), follows.unfollow);
 
     app.get('/api/users/:profileId/followers', passport.authenticate('jwt', {session: false}), follows.populateFollowers, follows.listFollowers);
 
     app.get('/api/users/:profileId/following', passport.authenticate('jwt', {session: false}), follows.populateFollowing, follows.listFollowing);
 
     app.param('profileId', users.userByID);
+    app.param('followRecord', follows.followRecord);
 };
